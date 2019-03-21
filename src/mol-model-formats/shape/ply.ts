7c1d6303c1702e67de9d313d2e79d4b2c0cbf1f2
MarcoSchaeferT <marco.schaefer@uni-tuebingen.de>
2019-03-21 18:25:55 +0100

    Revert "Ply test"

--- src/mol-model-formats/shape/ply.ts
+++ src/mol-model-formats/shape/ply.ts
@@ -6,76 +6,53 @@
  */
 
 import { RuntimeContext, Task } from 'mol-task';
+import { addTriangle } from 'mol-geo/geometry/mesh/builder/triangle';
 import { ShapeProvider } from 'mol-model/shape/provider';
 import { Color } from 'mol-util/color';
-import { PlyFile, PlyTable, PlyList } from 'mol-io/reader/ply/schema';
+import { PlyData, PlyFile } from 'mol-io/reader/ply/schema';
 import { MeshBuilder } from 'mol-geo/geometry/mesh/mesh-builder';
 import { Mesh } from 'mol-geo/geometry/mesh/mesh';
 import { Shape } from 'mol-model/shape';
-import { ChunkedArray } from 'mol-data/util';
 
-async function getPlyMesh(ctx: RuntimeContext, vertex: PlyTable, face: PlyList, mesh?: Mesh) {
-    const builderState = MeshBuilder.createState(face.rowCount, face.rowCount, mesh)
-    const { vertices, normals, indices, groups } = builderState
+async function getPlyMesh(ctx: RuntimeContext, centers: number[], normals: number[], faces: number[], mesh?: Mesh) {
+    const builderState = MeshBuilder.createState(faces.length, faces.length, mesh)
+    builderState.currentGroup = 0
+    for (let i = 0, il = faces.length/4; i < il; ++i) {
+        if (i % 10000 === 0 && ctx.shouldUpdate) await ctx.update({ current: i, max: il, message: `adding triangle ${i}` })
+        builderState.currentGroup = i
 
-    const x = vertex.getProperty('x')
-    const y = vertex.getProperty('y')
-    const z = vertex.getProperty('z')
-    if (!x || !y || !z) throw new Error('missing coordinate properties')
-
-    const nx = vertex.getProperty('nx')
-    const ny = vertex.getProperty('ny')
-    const nz = vertex.getProperty('nz')
-    if (!nx || !ny || !nz) throw new Error('missing normal properties')
-
-    const atomid = vertex.getProperty('atomid')
-    if (!atomid) throw new Error('missing atomid property')
-
-    for (let i = 0, il = vertex.rowCount; i < il; ++i) {
-        if (i % 10000 === 0 && ctx.shouldUpdate) await ctx.update({ current: i, max: il, message: `adding vertex ${i}` })
-
-        ChunkedArray.add3(vertices, x.value(i), y.value(i), z.value(i))
-        ChunkedArray.add3(normals, nx.value(i), ny.value(i), nz.value(i));
-        ChunkedArray.add(groups, atomid.value(i))
-    }
-
-    for (let i = 0, il = face.rowCount; i < il; ++i) {
-        if (i % 10000 === 0 && ctx.shouldUpdate) await ctx.update({ current: i, max: il, message: `adding face ${i}` })
-
-        const { entries } = face.value(i)
-        ChunkedArray.add3(indices, entries[0], entries[1], entries[2])
+        let triangle_vertices: number[];
+        let triangle_normals: number[];
+        let triangle_indices: number[];
+        triangle_vertices = [centers[faces[4*i+1]*3], centers[faces[4*i+1]*3+1], centers[faces[4*i+1]*3+2],
+                             centers[faces[4*i+2]*3], centers[faces[4*i+2]*3+1], centers[faces[4*i+2]*3+2],
+                             centers[faces[4*i+3]*3], centers[faces[4*i+3]*3+1], centers[faces[4*i+3]*3+2]];
+        triangle_normals = [ normals[faces[4*i+1]*3], normals[faces[4*i+1]*3+1], normals[faces[4*i+1]*3+2],
+                             normals[faces[4*i+2]*3], normals[faces[4*i+2]*3+1], normals[faces[4*i+2]*3+2],
+                             normals[faces[4*i+3]*3], normals[faces[4*i+3]*3+1], normals[faces[4*i+3]*3+2]];
+        triangle_indices = [0, 1, 2];
+        // console.log(triangle_vertices)
+        addTriangle(builderState, triangle_vertices, triangle_normals, triangle_indices)
     }
     return MeshBuilder.getMesh(builderState);
 }
 
-async function getShape(ctx: RuntimeContext, plyFile: PlyFile, props: {}, shape?: Shape<Mesh>) {
+async function getShape(ctx: RuntimeContext, parsedData: PlyData, props: {}, shape?: Shape<Mesh>) {
     await ctx.update('async creation of shape from  myData')
-
-    const vertex = plyFile.getElement('vertex') as PlyTable
-    if (!vertex) throw new Error('missing vertex element')
-
-    const atomid = vertex.getProperty('atomid')
-    if (!atomid) throw new Error('missing atomid property')
-
-    const red = vertex.getProperty('red')
-    const green = vertex.getProperty('green')
-    const blue = vertex.getProperty('blue')
-    if (!red || !green || !blue) throw new Error('missing color properties')
-
-    const face = plyFile.getElement('face') as PlyList
-    if (!face) throw new Error('missing face element')
-
-    const mesh = await getPlyMesh(ctx, vertex, face, shape && shape.geometry)
+    const { vertices, normals, faces, colors, properties } = parsedData
+    const mesh = await getPlyMesh(ctx, vertices, normals, faces, shape && shape.geometry)
     return shape || Shape.create(
-
-        'test', plyFile, mesh,
-
+        'ply-mesh', mesh,
         (groupId: number) => {
-            return Color.fromRgb(red.value(groupId), green.value(groupId), blue.value(groupId))
+            return Color.fromRgb(
+                colors[faces[4 * groupId + 1] * 3 + 0],
+                colors[faces[4 * groupId + 1] * 3 + 1],
+                colors[faces[4 * groupId + 1] * 3 + 2]
+            )
         },
         () => 1, // size: constant
         (groupId: number) => {
-            return atomid.value(groupId).toString()
+            return properties[parsedData.propertyCount * faces[4 * groupId + 1] + 10].toString()
         }
     )
 }
@@ -86,11 +63,11 @@ export const PlyShapeParams = {
 export type PlyShapeParams = typeof PlyShapeParams
 
 export function shapeFromPly(source: PlyFile, params?: {}) {
-    return Task.create<ShapeProvider<PlyFile, Mesh, PlyShapeParams>>('Parse Shape Data', async ctx => {
+    return Task.create<ShapeProvider<PlyData, Mesh, PlyShapeParams>>('Parse Shape Data', async ctx => {
         console.log('source', source)
         return {
             label: 'Mesh',
-            data: source,
+            data: source.PLY_File,
             getShape,
             geometryUtils: Mesh.Utils
         }

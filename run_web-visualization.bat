30db64d6c315c765f31cfaf5245698c1c28ee1e1
MarcoSchaeferT <Schaefer.marco.ms@web.de>
2020-06-08 11:52:24 +0200

    minor changes of ply-wrapper index.html

--- run_web-visualization.bat
+++ run_web-visualization.bat
@@ -1,4 +1,4 @@
 start cmd /k  http-server -p 5020
 ping -n 5 localhost> nul
-start chrome http://127.0.0.1:5020/build/src/examples/ply-wrapper/index.html
+start chrome http://127.0.0.1:5020/build/examples/ply-wrapper/index.html
 start cmd /k  npm run watch-extra
